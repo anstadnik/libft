58f6712af2a5efbf1159772ae0e043c2f846533e
Andrii STADNIK <astadnik@student.unit.ua>
2018-03-31 18:36:55 +0300

    Corrected ft_isinteger.c

--- is/srcs/ft_isinteger.c
+++ is/srcs/ft_isinteger.c
@@ -6,33 +6,43 @@
 /*   By: astadnik <astadnik@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/29 17:22:41 by astadnik          #+#    #+#             */
-/*   Updated: 2018/03/29 18:15:11 by astadnik         ###   ########.fr       */
+/*   Updated: 2018/03/31 18:15:09 by astadnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isinteger(const char	*s)
+static int	count(const char *s)
 {
-	char	neg;
-	int	counter;
+	int		counter;
 
-	neg = 0;
-	while (ft_iswhitespace(*s))
-		s++;
-	if (*s == '-' || *s == '+')
-		if (*s++ == '-')
-			neg = 1;
-	while (*s == '0')
-		s++;
 	counter = 0;
 	while (ft_isdigit(s[counter]))
 		if (counter > 9)
 			return (-1);
 		else
 			counter++;
-	if (!counter || (counter == 10 && (neg ? ft_strncmp(s, "2147483648", 10) :
-					ft_strncmp(s, "2147483647", 10)) > 0))
+	return (counter);
+}
+
+int	ft_isinteger(const char	*s)
+{
+	char	neg;
+	int		zero;
+	int		counter;
+
+	neg = 0;
+	zero = 0;
+	while (ft_iswhitespace(*s))
+		s++;
+	if (*s == '-' || *s == '+')
+		if (*s++ == '-')
+			neg = 1;
+	while (*s == '0' && zero++)
+		s++;
+	counter = count(s);
+	if ((neg ? ft_strncmp(s, "2147483648", 10) : ft_strncmp(s, "2147483647", 10)
+				> 0 && (counter == 10)) || (!counter && !zero))
 		return (-1);
 	return (neg);
 
